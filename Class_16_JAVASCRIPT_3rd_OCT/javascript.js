ffacc5593008c05b803f5c6bb96546b4ffcd6b2d
Ajinkya Lalge <ajinkya128@gmail.com>
2023-10-29 18:36:02 +0530

    Add files via upload

---
+++ Class_16_JAVASCRIPT_3rd_OCT/javascript.js
@@ -0,0 +1,54 @@
+
+
+var x = 10;
+
+var fname = "Ajinkya";
+
+var percentage = 77.77;
+
+var bulb = true;
+
+console.log(x);
+console.log(fname);
+console.log(percentage);
+console.log(bulb);
+
+
+
+// Math Operators
+
+a = 20;
+b = 10;
+c = 20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+// Comparion Operator
+
+console.log(a>b);
+console.log(a<b);
+
+console.log(a>=c);
+console.log(a<=c);
+
+console.log(a==b);
+console.log(a!=b);
+
+// Increment Decrement Operator
+
+// Preincremnet: We first increment the variable and then perform the operation
+//  Post increment: We first perform the operation and then perform the variable.
+
+var i = 10;
+
+var j = 5;
+
+var z = i++ - --i + j++;
+
+console.log(i , j , z);
+
+
